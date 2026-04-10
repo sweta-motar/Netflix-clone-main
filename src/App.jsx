ad4594bd35c3d7066e1dd367e250cb4063471559
sweta-motar <swetamotar@gmail.com>
2026-04-10 09:46:23 +0000

    done

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import { auth } from "./firebase";
 import { onAuthStateChanged } from "firebase/auth";
 
-import Home from "./pages/home";
+import Home from "./pages/Home";
 import Login from "./pages/login";
 import Profile from "./pages/Profile";
 import Wishlist from "./pages/Wishlist";
